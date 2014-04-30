91228528261e9d0caccbd38c9d0005b383e175d0
sagomilk <sagomilk@gmail.com>
2014-04-30 22:09:53 +0800

    10 MyTest finished

--- java/MyTest.java
+++ java/MyTest.java
@@ -1,11 +1,10 @@
-//17
+//20
 import java.awt.Frame;
-
 
 public class MyTest {
 	public static void main(String args[]) {
 		Frame w = new Frame();
-		w.setSize(200, 300);
+		w.setSize(300, 400);
 		w.show();
 	}
 }
