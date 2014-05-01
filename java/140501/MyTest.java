d90994928500bf4e009e5deef98ab171ef1c1e94
sagomilk <sagomilk@gmail.com>
2014-05-01 17:17:01 +0800

    17 MyPanel

--- java/140501/MyTest.java
+++ java/140501/MyTest.java
@@ -1,20 +1,22 @@
-//1 
+// 5
 import java.awt.*;
 
 public class MyTest {
 	public static void main(String[] args) {
-		Frame w = new Frame();
-		w.setSize(300, 400);
-
+		Frame w = new Frame();		
+		w.setSize(200, 300);
+		
 		MyPanel mp = new MyPanel();
 		w.add(mp);
-
+		
 		w.show();
 	}
 }
 
 class MyPanel extends Panel {
-	public void paint(Graphics g) {
-		g.drawLine(30, 30, 100, 100);
+	public void paint(Graphics g){
+		g.setColor(Color.BLUE);
+		g.fillOval(20, 20 ,100, 100);
+		g.drawLine(20, 20, 300, 300);
 	}
 }
