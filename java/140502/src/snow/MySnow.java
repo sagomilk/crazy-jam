8fe3d56036c0027f8ba96dbf9d1786ad592adfaf
sagomilk <sagomilk@gmail.com>
2014-05-02 15:29:01 +0800

    39 snow

---
+++ java/140502/src/snow/MySnow.java
@@ -0,0 +1,71 @@
+package snow;
+
+import java.awt.*;
+/*
+ * 漫天的雪花飘落
+ */
+
+public class MySnow {
+	public static void main(String[] args) {
+		Frame w = new Frame();
+		w.setSize(800, 600);
+		w.setBackground(Color.BLACK);
+		
+		
+		MyPanel mp = new MyPanel();
+		w.add(mp);
+		
+		Thread t = new Thread(mp);
+		t.start();
+		
+				
+		w.show();
+	}
+}
+
+class MyPanel extends Panel implements Runnable {
+	int[] x = new int[300];
+	int[] y = new int[300];
+	
+	public MyPanel() {
+		for (int i = 0; i < 300; i++) {
+			x[i] = (int)(800*Math.random());
+			y[i] = (int)(600*Math.random());
+		}
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.WHITE);
+		
+		for (int i = 0; i < 300; i++) {
+			g.drawString("*", x[i], y[i]);
+		}		
+		
+	}
+	
+	public void run() {
+		
+		while (true) {
+			
+			for (int i = 0; i < 300; i++) {
+				y[i] += 5*Math.random();
+				x[i] = x[i]+(int)(2*Math.random())-(int)(2*Math.random());
+				
+				if (y[i] > 600) {
+					x[i] = (int)(800*Math.random());
+					y[i] = (int)(100*Math.random());
+				}
+			}
+			
+
+			
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				
+			}
+			
+			repaint();
+		}
+	}
+}
