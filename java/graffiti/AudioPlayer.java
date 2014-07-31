1e0cd915de0436c575091b1eb497fa126a64cb12
sagomilk <sagomilk@gmail.com>
2014-08-01 01:16:49 +0800

    AudioPlayer

---
+++ java/graffiti/AudioPlayer.java
@@ -0,0 +1,13 @@
+class AudioPlayer {
+	protected boolean openSpeaker(Speaker sp) {
+		//
+	}
+}
+
+class StreamingAudioPlayer {
+	boolean openSpeaker(Speaker sp) {
+		//implement detail
+	}
+	public synchronized void showDetails() {
+	}
+}
